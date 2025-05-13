b31e17f7454ba03ae48b4bf799ff31d59f631366
kinosist <kinosist@hotmail.co.jp>
2025-05-13 19:08:23 +0900

    数字当てゲーム

---
+++ test.py
@@ -0,0 +1,13 @@
+import random
+
+com = random.randint(1, 100)
+
+while True:
+    user = int(input('数字を入力してください(1-100): '))
+    if user == com:
+        print('正解です！')
+        break
+    elif user < com:
+        print('もっと大きい数字です')
+    else:
+        print('もっと小さい数字です')
