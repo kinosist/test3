354db9970fce2805afcb3d2338e3dd170532e0aa
kinosist <kinosist@hotmail.co.jp>
2025-05-13 19:34:09 +0900

    add counter

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import random
 
 com = random.randint(1, 100)
 
+count = 0
 while True:
+    count = count + 1
     user = int(input('数字を入力してください(1-100): '))
     if user == com:
         print('正解です！')
+        print(count, '回目で正解しました')
         break
     elif user < com:
         print('もっと大きい数字です')
